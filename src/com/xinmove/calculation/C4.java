577b6467ad499865899e36f0a025c858664f84c3
chenweitao <chenweitao@zhiweidata.com>
2019-06-25 18:34:40 +0800

    C4doing

--- src/com/xinmove/calculation/C4.java
+++ src/com/xinmove/calculation/C4.java
@@ -1,12 +1,50 @@
 package com.xinmove.calculation;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * @ClassName C4
  * @Descripption TODO
  * @Author CWT
- * @Date 2019/6/25 17:56
+ * @Date 2019/6/25 18:56
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+ * <p>
+ * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ * <p>
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ * <p>
+ * 示例 1:
+ * <p>
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ * <p>
+ * 则中位数是 2.0
+ * 示例 2:
+ * <p>
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ * <p>
+ * 则中位数是 (2 + 3)/2 = 2.5
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  **/
 public class C4 {
 
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        List<Integer> list = new LinkedList(Arrays.asList(nums1));
+
+        int index = 0;
+        for (int i = 0; i <nums1.length; i++) {
+
+        }
+
+
+        return 0;
+    }
 
 }
